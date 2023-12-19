cec30bf0360721254ba138af970880e60a700843
Dambaev Alexander <ice.redmine@gmail.com>
2023-12-20 01:17:27 +0800

    feat: enable prometheus in order to monitor disk space and op-energy metrics
    mainnet volume defined by mainnet_volume variable
    
    alerts are being sent to telegram channel defined by tg-alerts-chat-id,
    tg-alerts-bot-token:
    - 10 GiB alerts are of average severity;
    - 5 GiB alerts are of critical secerity.

--- host.nix
+++ host.nix
@@ -12,6 +12,9 @@ env@{
 , bitcoind-mainnet-rpc-pskhmac ? builtins.readFile ( "/etc/nixos/private/bitcoind-mainnet-rpc-pskhmac.txt")
 , op-energy-db-psk-mainnet ? builtins.readFile ( "/etc/nixos/private/op-energy-db-psk-mainnet.txt")
 , op-energy-db-salt-mainnet ? builtins.readFile ( "/etc/nixos/private/op-energy-db-salt-mainnet.txt")
+, tg-alerts-chat-id ? builtins.readFile ("/etc/nixos/private/op-energy-tg-alerts-chat-id")
+, tg-alerts-bot-token ? builtins.readFile ("/etc/nixos/private/op-energy-tg-alerts-bot-token")
+, mainnet_volume ? builtins.readFile ("/etc/nixos/private/mainnet-volume")
 , mainnet_node_ssh_tunnel ? true # by default we want ssh tunnel to main node, but this is useless for github actions as they are using only signet node
 }:
 {pkgs, lib, ...}:
@@ -29,6 +32,55 @@ let
       ''
     );
   opEnergyModule = import ./overlays/op-energy/nix/module.nix { GIT_COMMIT_HASH = GIT_COMMIT_HASH; };
+  btc_volume_alert = pkgs.writeText "btc_volume_alert" ''
+    groups:
+    - name: node.rules
+      rules:
+      - alert: mainnet volume free space is less than 10 GiB
+        expr: node_filesystem_avail_bytes{mountpoint="${mainnet_volume}"} < 10737418240
+        for: 5m
+        labels:
+          severity: average
+      - alert: mainnet volume free space is less than 5 GiB
+        expr: node_filesystem_avail_bytes{mountpoint="${mainnet_volume}"} < 5368709120
+        for: 5m
+        labels:
+          severity: critical
+  '';
+  telegram_template = pkgs.writeText "telegram_template" ''
+    {{ define "telegram.default" }}
+    {{ range .Alerts }}
+    {{ if eq .Status "firing"}}&#x1F525<b>{{ .Status | toUpper }}</b>&#x1F525{{ else }}&#x2705<b>{{ .Status | toUpper }}</b>&#x2705{{ end }}
+    <b>{{ .Labels.alertname }}</b>
+    {{- if .Labels.severity }}
+    <b>Severity:</b> {{ .Labels.severity }}
+    {{- end }}
+    {{- if .Labels.ds_name }}
+    <b>Database:</b> {{ .Labels.ds_name }}
+    {{- if .Labels.ds_group }}
+    <b>Database group:</b> {{ .Labels.ds_group }}
+    {{- end }}
+    {{- end }}
+    {{- if .Labels.ds_id }}
+    <b>Cluster UUID: </b>
+    <code>{{ .Labels.ds_id }}</code>
+    {{- end }}
+    {{- if .Labels.instance }}
+    <b>instance:</b> {{ .Labels.instance }}
+    {{- end }}
+    {{- if .Annotations.message }}
+    {{ .Annotations.message }}
+    {{- end }}
+    {{- if .Annotations.summary }}
+    {{ .Annotations.summary }}
+    {{- end }}
+    {{- if .Annotations.description }}
+    {{ .Annotations.description }}
+    {{- end }}
+    {{ end }}
+    {{ end }}
+  '';
+
 in
 {
   imports = [
@@ -117,4 +169,76 @@ in
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
   networking.firewall.logRefusedConnections = false; # we are not interested in a logs of refused connections
+  # bitcoin storage monitoring
+  services.prometheus = {
+    enable = true;
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [
+          {
+            targets = [
+              "localhost:9100"
+            ];
+          }
+        ];
+      }
+    ];
+    exporters.node = {
+      enable = true;
+    };
+    ruleFiles = [
+      btc_volume_alert
+    ];
+    alertmanagers = [
+      { scheme = "http";
+        path_prefix = "/";
+        static_configs = [
+          { targets = [
+              "localhost:9093"
+            ];
+          }
+        ];
+      }
+    ];
+    alertmanager = {
+      enable = true;
+      port = 9093;
+      configText = ''
+        global:
+         resolve_timeout: 5m
+         telegram_api_url: "https://api.telegram.org"
+
+        templates:
+          - '${telegram_template}'
+
+        receivers:
+         - name: blackhole
+         - name: telegram-test
+           telegram_configs:
+            - chat_id: ${tg-alerts-chat-id}
+              bot_token: "${tg-alerts-bot-token}"
+              api_url: "https://api.telegram.org"
+              send_resolved: true
+              parse_mode: HTML
+              message: '{{ template "telegram.default" . }}'
+
+
+        route:
+         group_by: ['ds_id']
+         group_wait: 15s
+         group_interval: 30s
+         repeat_interval: 12h
+         receiver: telegram-test
+         routes:
+          - receiver: telegram-test
+            continue: true
+            matchers:
+             - severity=~"average|critical"
+          - receiver: blackhole
+            matchers:
+             - alertname="Watchdog"
+      '';
+    };
+  };
 }
